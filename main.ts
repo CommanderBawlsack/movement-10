4ed46bc06cf57d0e1281b4ee3133772821fc1054
CommanderBawlsack <mattkjaer11@gmail.com>
2025-02-03 17:56:02 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("welcome to the matrix")
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
